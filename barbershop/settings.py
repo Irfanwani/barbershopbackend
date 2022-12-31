6149c53f4aafc7e6bb60d07228575545b883b83b
Irfanwani <irfanwani347@gmail.com>
2022-12-31 12:22:26 +0530

    added domain

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['barbershopbackend.herokuapp.com', '10.0.2.2', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1', 'localhost', 'barbershopservices.pythonanywhere.com/']
 
 
 if os.name == 'nt':
